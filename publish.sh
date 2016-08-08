12800fd16e582786141d4d05f334913b39344b36
khashab2 <khashab2@illinois.edu>
2016-08-07 18:58:12 -0700

    minor change

--- publish.sh
+++ publish.sh
@@ -24,14 +24,15 @@ fi
 # change the version number 
 bash increment-pom-versions.sh
 
-
 # setup the authentication required for deploy
 # start the ssh-agent in the background
 eval "$(ssh-agent -s)"
 ssh-add /home/runner/.ssh/ai2
 
 # add to the known hosts 
-ssh -o StrictHostKeyChecking=no khashab2@bilbo.cs.illinois.edu
+# ssh-keygen -R bilbo.cs.illinois.edu
+ssh-keyscan -t rsa,dsa bilbo.cs.illinois.edu 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts
+mv ~/.ssh/tmp_hosts ~/.ssh/known_hosts
 
 # going to deploy 
 mvn deploy
